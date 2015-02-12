f711b38f18a13837eab953d6f60dbc618b10ba1a
hirosh7 <hirosh7@gmail.com>
2015-02-12 00:12:11 -0700

    Implemented code corresponding to Connecting the List to the Adapter, and Updating the List Data. Book query app now works

--- app/src/main/java/com/messengerapp/messengerapp/JSONAdaptor.java
+++ app/src/main/java/com/messengerapp/messengerapp/JSONAdaptor.java
@@ -8,9 +8,12 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.json.JSONArray;
+import com.squareup.picasso.Picasso;
 
- // Created by hirosh7 on 2/11/2015.
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+// Created by hirosh7 on 2/11/2015.
 
 public class JSONAdaptor extends BaseAdapter {
     private static final String IMAGE_URL_BASE = "http://covers.openlibrary.org/b/id/";
@@ -51,7 +54,7 @@ public class JSONAdaptor extends BaseAdapter {
         if (convertView == null) {
 
             // Inflate the custom row layout from your XML.
-            convertView = mInflater.inflate(R.layout.row_book, null);
+            convertView = mInflater.inflate(R.layout.row_book, parent, false);
 
             // create a new "Holder" with subviews
             holder = new ViewHolder();
@@ -67,7 +70,42 @@ public class JSONAdaptor extends BaseAdapter {
             // and just get the holder you already made
             holder = (ViewHolder) convertView.getTag();
         }
-        // More code after this
+        // Get the current book's data in JSON form
+        JSONObject jsonObject = (JSONObject) getItem(position);
+
+        // See if there is a cover ID in the Object
+        if (jsonObject.has("cover_i")) {
+
+            // If so, grab the Cover ID out from the object
+            String imageID = jsonObject.optString("cover_i");
+
+            // Construct the image URL (specific to API)
+            String imageURL = IMAGE_URL_BASE + imageID + "-L.jpg";
+
+            // Use Picasso to load the image
+            // Temporarily have a placeholder in case it's slow to load
+            Picasso.with(mContext).load(imageURL).placeholder(R.drawable.ic_books).into(holder.thumbnailImageView);
+        } else {
+
+            // If there is no cover ID in the object, use a placeholder
+            holder.thumbnailImageView.setImageResource(R.drawable.ic_books);
+        }
+
+        // Grab the title and author from the JSON
+        String bookTitle = "";
+        String authorName = "";
+
+        if (jsonObject.has("title")) {
+            bookTitle = jsonObject.optString("title");
+        }
+
+        if (jsonObject.has("author_name")) {
+            authorName = jsonObject.optJSONArray("author_name").optString(0);
+        }
+
+        // Send these Strings to the TextViews for display
+        holder.titleTextView.setText(bookTitle);
+        holder.authorTextView.setText(authorName);
 
         return convertView;
     }
@@ -78,5 +116,11 @@ public class JSONAdaptor extends BaseAdapter {
         public ImageView thumbnailImageView;
         public TextView titleTextView;
         public TextView authorTextView;
+    }
+
+    public void updateData(JSONArray jsonArray) {
+        // update the adapter's dataset
+        mJsonArray = jsonArray;
+        notifyDataSetChanged();
     }
 }
