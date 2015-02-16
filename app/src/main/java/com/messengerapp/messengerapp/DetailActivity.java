f9bb1afc316ce05f9686ff010f2a12575b73d101
hirosh7 <hirosh7@gmail.com>
2015-02-15 22:21:35 -0700

    Implemented sharing the book cover as a book recommendation - tutorial complete!

--- app/src/main/java/com/messengerapp/messengerapp/DetailActivity.java
+++ app/src/main/java/com/messengerapp/messengerapp/DetailActivity.java
@@ -2,8 +2,13 @@
 
 package com.messengerapp.messengerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -12,6 +17,7 @@ public class DetailActivity extends ActionBarActivity {
 
     private static final String IMAGE_URL_BASE = "http://covers.openlibrary.org/b/id/"; // 13
     String mImageURL; // 13
+    ShareActionProvider mShareActionProvider; // 14
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,5 +44,41 @@ public class DetailActivity extends ActionBarActivity {
             // Use Picasso to load the image
             Picasso.with(this).load(mImageURL).placeholder(R.drawable.img_books_loading).into(imageView);
         }
+    }
+
+    private void setShareIntent() {
+
+        // create an Intent with the contents of the TextView
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT,
+                "Book Recommendation!");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mImageURL);
+
+        // Make sure the provider knows
+        // it should work with that Intent
+        mShareActionProvider.setShareIntent(shareIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu
+        // this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Access the Share Item defined in menu XML
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        // Access the object responsible for
+        // putting together the sharing sub-menu
+        if (shareItem != null) {
+            mShareActionProvider
+                    = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        }
+
+        setShareIntent();
+
+        return true;
     }
 }
